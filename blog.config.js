9a130ea0caf450e9e5de5c87c7b3f6028fc6f28c
ruhuang <ruhuang2001@gmail.com>
2024-04-22 15:14:40 +0800

    Fix the rss error

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'ruhuang blog',
   author: 'ruhuang',
   email: 'ruhuang2001@gmail.com',
-  link: 'https://nobelium.vercel.app',
+  link: 'https://ruhuang2001.vercel.app/',
   description: 'ruhuang website about life and code.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
