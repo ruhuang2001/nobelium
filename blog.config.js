dce34c32a163020683236e3c62da89fd392c8396
ruhuang2001 <ruhuang2001@gmail.com>
2024-04-28 16:12:18 +0800

    fix a sentence and add some seo

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://cn.gravatar.com/ruhuang2001',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', 'Website', 'Notion', "ruhuang", "ruhuang2001"],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
