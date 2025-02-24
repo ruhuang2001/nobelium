8636aed59052c6f25c6b21d73ca8a82c011ec74b
ruhuang <ruhuang2001@gmail.com>
2025-02-24 10:28:41 +0800

    feat(sitemap): support sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteUrl: config.link,
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  generateIndexSitemap: false
+  generateIndexSitemap: true
   // ...other options
   // https://github.com/iamvishnusankar/next-sitemap#configuration-options
 }
